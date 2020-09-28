37f71ffc4c5bcb93aebebf3b27e12a86b03ea08e
Pedro Luis Holguin <dropeholguin1994@gmail.com>
2020-09-28 11:36:54 -0500

    Se agrego query para obtener las imagenes de un producto

--- graphql/schemas/querys/index.js
+++ graphql/schemas/querys/index.js
@@ -1,11 +1,13 @@
 const WooCommerceProduct = require('./WooCommerce/wooCommerceProduct.query');
 const WooCommerceProductVariation = require('./WooCommerce/wooCommerceProductVariation.query');
+const WooCommerceProductImage = require('./WooCommerce/wooCommerceProductImage.query');
 const ShopifyProducts = require('./Shopify/shopifyProduct.query.js');
 
 module.exports = {
     public: {
         WooCommerceProduct,
         WooCommerceProductVariation,
+        WooCommerceProductImage,
         ShopifyProducts,
     }
 }
