f304c9a4271507f29e56d6796e67c8b750d6592e
Pedro Luis Holguin <dropeholguin1994@gmail.com>
2020-12-11 14:55:10 -0500

    se agrega suscripcion para vtex

--- graphql/schemas/subscriptions/index.js
+++ graphql/schemas/subscriptions/index.js
@@ -1,11 +1,14 @@
 const { GraphQLObjectType } = require('graphql');
 const ShopifyProducts = require('./Shopify/shopifyProducts');
+const VtexProducts = require('./Vtex/vtexProducts');
+
 
 const Subscription = new GraphQLObjectType({
   name: 'RootSubscription',
   description: 'Root Subscription',
   fields: {
     ShopifyProducts: ShopifyProducts,
+    VtexProducts: VtexProducts
   }
 });
 
