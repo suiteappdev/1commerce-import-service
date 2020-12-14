b3552447b10150f72fe35f8c9363f33e9c097f0f
Andrew Castro <listerine1989@gmail.com>
2020-12-14 16:10:05 -0500

    fix event

--- routes/wooCommerce.route.js
+++ routes/wooCommerce.route.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const { pubsub }  = require('../services/pubsub.service');
-const { WOOCOMMERCE_PRODUCT_CREATED }  = require('../graphql/schemas/subscriptions/events');
+const { WOOCOMMERCE_PRODUCTS }  = require('../graphql/schemas/subscriptions/events');
 
-router.post('/hook/woocommerce/:key', async (req, res)=>{
+router.post('/created_product/woocommerce/:key', async (req, res)=>{
     let key = req.params.key;
     let data = {
       productId: req.body.id,
       key,
       channel: 'woocommerce'
     };
-    pubsub.publish(WOOCOMMERCE_PRODUCT_CREATED, { WoocommerceProductCreated: data });
+    console.log("wooCommerc hook body", data);
+    pubsub.publish(WOOCOMMERCE_PRODUCTS, { WoocommerceProductCreated: data });
     res.json(data);
 });
 
