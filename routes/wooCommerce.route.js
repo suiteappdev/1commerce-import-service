829f84dc3109616ef368c59fb753829ca505438c
Andrew Castro <listerine1989@gmail.com>
2020-12-14 16:36:28 -0500

    latest

--- routes/wooCommerce.route.js
+++ routes/wooCommerce.route.js
@@ -4,6 +4,7 @@ const { pubsub }  = require('../services/pubsub.service');
 const { WOOCOMMERCE_PRODUCTS }  = require('../graphql/schemas/subscriptions/events');
 
 router.post('/created_product/woocommerce/:key', async (req, res)=>{
+    console.log(req.body);
     let key = req.params.key;
     let data = {
       productId: req.body.id,
