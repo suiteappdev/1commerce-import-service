74aa5721c6f7db67cbecb34843f8003cc5d2faba
David <felipe0160@gmail.com>
2020-12-15 09:59:47 -0500

    prestashop, rutas, tipos y eventos

---
+++ routes/prestashop.route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const { pubsub }  = require('../services/pubsub.service');
+const { PRESTASHOP_PRODUCTS }  = require('../graphql/schemas/subscriptions/events');
+
+router.post('/prestashop/createproduct/:key', async (req, res)=>{
+  let key = req.params.key;
+  let data = {
+    productId: req.body.id,
+    key,
+    channel: 'prestashop'
+  };
+  pubsub.publish(PRESTASHOP_PRODUCTS, { PrestashopProducts: data });
+  res.json(data);
+});
+
+router.post('/prestashop/updateproduct/:key', async (req, res)=>{
+  let key = req.params.key;
+  let data = {
+    productId: req.body.id,
+    key,
+    channel: 'prestashop'
+  };
+  pubsub.publish(PRESTASHOP_PRODUCTS, { ShopifyProducts: data });
+  res.json(data);
+});
+
+module.exports = router;
