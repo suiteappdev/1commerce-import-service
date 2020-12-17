6f11ef457d362bb2f0362aaabdaf8466e8bc47d0
David <felipe0160@gmail.com>
2020-12-17 15:20:37 -0500

    Se deshabilita ruta de creacion de productos

--- routes/prestashop.route.js
+++ routes/prestashop.route.js
@@ -3,20 +3,7 @@ const router = express.Router();
 const { pubsub }  = require('../services/pubsub.service');
 const { PRESTASHOP_PRODUCTS }  = require('../graphql/schemas/subscriptions/events');
 
-router.post('/prestashop/createproduct/:key', async (req, res)=>{
-  console.log(req.body);
-  let key = req.params.key;
-  let data = {
-    productId: req.body.id_product,
-    key,
-    channel: 'prestashop'
-  };
-  pubsub.publish(PRESTASHOP_PRODUCTS, { PrestashopProducts: data });
-  res.json(data);
-});
-
 router.post('/prestashop/updateproduct/:key', async (req, res)=>{
-  console.log(req.body);
   let key = req.params.key;
   let data = {
     productId: req.body.id_product,
